6c301da0640ad99c7e2c4f4b632fc7b0d5224787
njmeister <150966120+njmeister@users.noreply.github.com>
2024-09-21 17:23:27 -0500

    added files back in

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,40 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import App from './App';
 import './index.css';
+import playRoutes from './routes/playRoutes';
 
+// Dynamically import components
 const Homepage = lazy(() => import('./pages/Homepage'));
+const SelectGamePage = lazy(() => import('./pages/SelectGamePage'));
 
+// Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		children: [
-			{
-				path: '/',
-				element: <Suspense fallback={<div>Loading...</div>}><Homepage /></Suspense>,
-			},
-		],
-	},
+  {
+    path: '/',
+    element: <App />,
+    children: [
+    //   {
+    //     path: '/',
+    //     element: (
+    //       <Suspense fallback={<div>Loading...</div>}>
+    //         <Homepage />
+    //       </Suspense>
+    //     ),
+    //   },
+      ...playRoutes,
+      {
+        path: '*',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <SelectGamePage />
+          </Suspense>
+        ),
+      },
+    ],
+  },
 ]);
 
+// Render the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<RouterProvider router={router} />
-  );
+  <RouterProvider router={router} />
+);
