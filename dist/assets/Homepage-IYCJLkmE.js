f5c4e46d37a51ddad34104b9c649b14e270a2e45
njmeister <150966120+njmeister@users.noreply.github.com>
2024-09-21 16:06:35 -0500

    github pages branch

---
+++ dist/assets/Homepage-IYCJLkmE.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BVpHL2tz.js";function o(){return e.jsx("div",{children:e.jsx("h1",{children:"Homepage"})})}export{o as default};
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/UniqueAdventures/', // Add this line
+  base: '/UniqueAdventures/',
   plugins: [react()],
   server: {
     port: 3000,
